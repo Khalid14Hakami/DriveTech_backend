b36cfdeb5e59fb4735a783ef14ce29300fc66f7d
KHALID HAKAMI <khalid.hakami14@gmail.com>
2023-04-26 14:20:04 +0200

    added requirements files

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     if ARGS.debug:
         print("Running in debug mode")
         CORS = CORS(APP)
-        APP.run(host='0.0.0.0', port=PORT, debug=True)
+        # APP.run(host='0.0.0.0', port=PORT, debug=True)
     else:
-        APP.run(host='0.0.0.0', port=PORT, debug=False)
-
+        # APP.run(host='0.0.0.0', port=PORT, debug=False)
+        pass
