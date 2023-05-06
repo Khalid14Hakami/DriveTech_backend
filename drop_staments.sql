bf138196d414430cfbac76f83cb87d25106ce9d4
KHALID HAKAMI <khalid.hakami14@gmail.com>
2023-05-06 16:22:33 +0300

    car log service

---
+++ drop_staments.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `CAR`;                        
+DROP TABLE IF EXISTS `CAR_LOG`;                    
+DROP TABLE IF EXISTS `ROUTINE`;                    
+DROP TABLE IF EXISTS `STORAGE`;                    
+DROP TABLE IF EXISTS `STORED`;                     
+DROP TABLE IF EXISTS `TASK`;                       
+DROP TABLE IF EXISTS `USER`;                       
+DROP TABLE IF EXISTS `rtn_tsk`; 
